513130ad493bd8f758a6576f4f6101076e3a9d5e
Chris C <ctcarrier@gmail.com>
2024-03-16 21:46:27 -0400

    tf: Migrate to current version of providers

--- main.tf
+++ main.tf
@@ -1,82 +1,115 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 1.7.5"
-    }
-  }
-}
-
 provider "aws" {  
   region  = var.aws_region
 }
 
-resource "aws_vpc" "stateless_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.stateless_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = var.availability_zones[0]
-}
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.stateless_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = var.availability_zones[1]
-}
-
-resource "aws_security_group" "stateless_sg" {
-  name        = "stateless-sg"
-  description = "Security group for EKS cluster"
-  vpc_id      = aws_vpc.stateless_vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+# Filter out local zones, which are not currently supported 
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
 }
 
-resource "aws_eks_cluster" "stateless_cluster" {
-  name     = "stateless-cluster"
-  role_arn = aws_iam_role.stateless_cluster_role.arn
+locals {
+  cluster_name = "stateless-eks-${random_string.suffix.result}"
+}
 
-  vpc_config {
-    subnet_ids         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-    security_group_ids = [aws_security_group.stateless_sg.id]
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "stateless-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster_attach_policy, aws_iam_role.stateless_cluster_role]
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
 }
 
-resource "aws_iam_role" "stateless_cluster_role" {
-  name = "stateless-cluster-role"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
 
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "eks.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  })
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_attach_policy" {
-  role       = aws_iam_role.stateless_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
 }
